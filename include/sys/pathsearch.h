142cb3981825ec412a0822f0c0df6270712fbe1f
Harkaitz Agirre <harkaitzv@gmail.com>
2022-06-13 01:15:47 +0200

    Fixed last path ignored bug in sys/pathsearch.h

--- include/sys/pathsearch.h
+++ include/sys/pathsearch.h
@@ -7,30 +7,26 @@
 #include <string.h>
 #include <syslog.h>
 #include <errno.h>
+#include <stdio.h>
 
 static __attribute__((unused))
 bool pathsearch (const char _path[], char _sep, const char _name[], char **_o) {
-    size_t      l_fil  = strlen(_name);
-    const char *s1     = _path;
-    const char *s2     = strchr(s1, _sep);
     char       *found  = NULL;
     bool        retval = false;
-    while(s1 && s2) {
-        size_t l_dir = (s2-s1);
-        size_t l_buf = l_dir+1+l_fil+1;
-        char b[l_buf];
-        memcpy(b, s1, l_dir);
-        b[l_dir] = '/';
-        memcpy(b+l_dir+1, _name, l_fil+1);
-        struct stat sb;
-        if (stat(b, &sb) == 0 && sb.st_mode & S_IXUSR) {
-            found = malloc(l_buf);
+    char        path[strlen(_path)+1];
+    char        seps[] = {_sep, '\0'};
+    strcpy(path, _path);
+
+    
+    for(char *s2,*s1 = strtok_r(path, seps, &s2); s1; s1 = strtok_r(NULL, seps, &s2)) {
+        char b[strlen(s1)+1+strlen(_name)+1];
+        sprintf(b, "%s/%s", s1, _name);
+        struct stat sb = {0};
+        if ((stat(b, &sb) == 0) && (sb.st_mode & S_IXUSR)) {
+            found = strdup(b);
             if (!found/*err*/) goto cleanup_errno;
-            memcpy(found, b, l_buf);
             break;
         }
-        s1 = s2+1;
-        s2 = strchr(s1, _sep);
     }
     if (!found/*err*/) goto cleanup_not_found;
     if (_o) {
@@ -38,16 +34,16 @@ bool pathsearch (const char _path[], char _sep, const char _name[], char **_o) {
         found = NULL;
     }
     retval = true;
-    goto cleanup;
+ cleanup:
+    free(found);
+    return retval;
  cleanup_errno:
     syslog(LOG_ERR, "%s: %s", _name, strerror(errno));
     goto cleanup;
  cleanup_not_found:
     syslog(LOG_ERR, "%s: Not found.", _name);
     goto cleanup;
- cleanup:
-    free(found);
-    return retval;
+ 
 }
 
 #ifndef PATH_SEP
